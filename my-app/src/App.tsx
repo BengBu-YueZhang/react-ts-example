9cd2ccec5441fb56ec782972b804f033aa7e9695
BengBu-YueZhang <zhangyue_coder@163.com>
2018-12-12 14:44:52 +0800

    update

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,17 +3,20 @@ import './App.css';
 
 import Counter from './components/Counter';
 import HelloReactTs from './components/HelloReactTs';
-
+import store from './store';
+import { Provider } from 'react-redux';
 
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <HelloReactTs
-          name={'赵晨'}
-        />
-        <Counter/>
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <HelloReactTs
+            name={'赵晨'}
+          />
+          <Counter/>
+        </div>
+      </Provider>
     );
   }
 }
