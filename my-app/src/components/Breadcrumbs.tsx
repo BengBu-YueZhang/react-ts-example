5fa13e84cedd75956a68c61a5096c54a6c231b53
BengBu-YueZhang <zhangyue_coder@163.com>
2018-12-13 20:22:05 +0800

    update

---
+++ my-app/src/components/Breadcrumbs.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { Breadcrumb } from 'antd';
+import * as ReactRouterDom from 'react-router-dom';
+import RouterMap from '../config/routers';
+
+export interface Location {
+  pathname: string;
+  [key: string]: any;
+} 
+
+const withRouter = ReactRouterDom.withRouter;
+const Link = ReactRouterDom.Link;
+
+class Breadcrumbs extends React.Component<Location, object> {
+  public render () {
+    return null
+  }
+}
+
+export default Breadcrumbs
