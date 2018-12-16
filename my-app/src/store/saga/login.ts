c2b872f73d3af1f0872813d9898f58a3b23e767e
BengBu-YueZhang <zhangyue_coder@163.com>
2018-12-16 10:13:47 +0800

    update

--- my-app/src/store/saga/login.ts
+++ my-app/src/store/saga/login.ts
@@ -1,24 +1,41 @@
 import * as Request from '../../util/request';
 import * as LoginActions from '../actions/login';
-import { setSessionStorage, removeSessionStorage, isHaveSessionStorage } from '../../util/storage';
+import * as SpinActions from '../actions/spin';
+import { setSessionStorage, clearSessionStorage, isHaveSessionStorage } from '../../util/storage';
 import { take, fork, put, call } from 'redux-saga/effects';
 
-function* authorize(): any {
+// 登录
+function* authorize(username: string, password: string, callback: () => void): any {
   try {
+    yield put(SpinActions.loadingStart(SpinActions.SpinType.Global));
+    const { token } = yield call(Request.login);
+    yield call(setSessionStorage, 'token', token);
+    yield call(callback)
   } catch (error) {
+    // ...
+    // 暂时不做处理, 应当给予消息提示
   } finally {
+    yield put(SpinActions.loadingEnd(SpinActions.SpinType.Global));
   }
 }
 
+// 登出
 function* logout(): any {
   try {
+    yield put(SpinActions.loadingStart(SpinActions.SpinType.Global));
+    yield call(clearSessionStorage);
+    yield call(Request.logout);
   } catch (error) {
+    // ...
+    // 暂时不做处理, 应当给予消息提示
   } finally {
+    yield put(SpinActions.loadingEnd(SpinActions.SpinType.Global));
   }
 }
 
-export function* main(): any {
+export default function* main(): any {
   while (true) {
+    // 如果不存在token, 需要重新监听LOGIN_REQUEST的action
     if (!isHaveSessionStorage('token')) {
       const { username, password, callback } = yield take(LoginActions.LOGIN_REQUEST);
       yield fork(authorize, username, password, callback);
